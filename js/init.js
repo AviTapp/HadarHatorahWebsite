cf979d04019681ef0b850e1d5447c7bda6847098
Avi Tapp <Andrew.tapp@gmail.com>
2018-10-18 13:18:27 -0400

    Update js/init.js
    added dropdown function and collapsible function

--- js/init.js
+++ js/init.js
@@ -1,8 +1,10 @@
-(function($){
-  $(function(){
+(function ($) {
+  $(function () {
 
     $('.sidenav').sidenav();
     $('.parallax').parallax();
+    $('.dropdown-trigger').dropdown();
+    $('.collapsible').collapsible();
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
